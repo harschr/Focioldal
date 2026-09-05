99bd7d98d4164403c637e019b9fa0e94e55678dc
harschrobert <harschr@gmail.com>
2026-09-05 21:51:02 +0200

    navbar v5

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -7,12 +7,12 @@ import { NbII } from './nb-ii/nb-ii';
 import { Hiba } from './hiba/hiba';
 
 const routes: Routes = [
+{path:"", redirectTo: "manutd", pathMatch:"full"},
 {path:"manutd", component: ManUtd},
 {path: "pl", component: PL},
 {path: "vidi", component: Vidi},
 {path: "nbII", component: NbII},
-/*{path:"", redirectTo: "/manutd", pathMatch:"full"},
-{path:"**", component: Hiba}*/
+{path:"**", component: Hiba}
 ];
 
 @NgModule({
